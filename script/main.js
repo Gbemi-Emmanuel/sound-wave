69118e3f65b2fa734e1513338893071e31b7fd82
Gbemi-Emmanuel <109102914+Gbemi-Emmanuel@users.noreply.github.com>
2022-08-22 14:34:11 +0100

    updated

--- script/main.js
+++ script/main.js
@@ -229,14 +229,342 @@ darkModeBody.addEventListener("click", () => {
 
 
 //======== TRENDING DOWNLOAD =======
+var Trending = document.getElementById("trending");
+var Recently = document.getElementById("recently");
+var Local = document.getElementById("local");
+var Foreign = document.getElementById("foreign");
+var LandingPage = document.getElementById("landingPage");
+var navBar = document.getElementById("navBar");
+var downloadPage = document.getElementById("download");
+var downloadImg = document.getElementById("downloadImg");
+var downloadBtn = document.getElementById("downloadBtn");
 
-tdownloadMusic = () => {
-//  let songTitle1 = document.getElementById("songTitle1");
-//  let songTitleText1 = document.getElementById("songTitleText1");
-//  let downloadBtn = document.getElementById("downloadBtn");
+downloadGoBack = () => {
 
- document.getElementById("downloadH4").innerHTML = "Bandana";
- document.getElementById("downloadP").innerHTML = "Fireboy DML (Ft. Asake)";
-
+    LandingPage.style.display = "block";
+    Recently.style.display = "block";
+    Foreign.style.display = "block";
+    Trending.style.display = "block";
+    navBar.style.display = "block";
+    downloadPage.style.display = "none";
 }
 
+
+
+// Bandana
+tdownloadMusic1 = () => {
+
+    let tSongTitle1 = document.getElementById("tSongTitle1");
+    let tSongTitleText1 = document.getElementById("tSongTitleText1");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle1.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText1.innerHTML;
+    downloadBtn.href = "./audio/trending/Bandana.mp3";
+    downloadAudio.src = "./audio/trending/Bandana.mp3";
+    downloadImg.src = document.getElementById("trendingImg1").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+// Machala
+tdownloadMusic2 = () => {
+
+    let tSongTitle2 = document.getElementById("tSongTitle2");
+    let tSongTitleText2 = document.getElementById("tSongTitleText2");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle2.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText2.innerHTML;
+    downloadBtn.href = "./audio/trending/Machala.mp3";
+    downloadAudio.src = "./audio/trending/Machala.mp3";
+    downloadImg.src = document.getElementById("trendingImg2").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//its plenty
+tdownloadMusic3 = () => {
+
+    let tSongTitle3 = document.getElementById("tSongTitle3");
+    let tSongTitleText3 = document.getElementById("tSongTitleText3");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle3.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText3.innerHTML;
+    downloadBtn.href = "./audio/trending/itsplenty.mp3";
+    downloadAudio.src = "./audio/trending/itsplenty.mp3";
+    downloadImg.src = document.getElementById("trendingImg3").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//common person
+tdownloadMusic4 = () => {
+
+    let tSongTitle4 = document.getElementById("tSongTitle4");
+    let tSongTitleText4 = document.getElementById("tSongTitleText4");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle4.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText4.innerHTML;
+    downloadBtn.href = "./audio/trending/Bandana.mp3";
+    downloadAudio.src = "./audio/trending/Bandana.mp3";
+    downloadImg.src = document.getElementById("trendingImg4").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//peace be unto you
+tdownloadMusic5 = () => {
+
+    let tSongTitle5 = document.getElementById("tSongTitle5");
+    let tSongTitleText5 = document.getElementById("tSongTitleText5");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle5.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText5.innerHTML;
+    downloadBtn.href = "./audio/trending/pbuy.mp3";
+    downloadAudio.src = "./audio/trending/pbuy.mp3";
+    downloadImg.src = document.getElementById("trendingImg5").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//girlfriend
+tdownloadMusic6 = () => {
+
+    let tSongTitle6 = document.getElementById("tSongTitle6");
+    let tSongTitleText6 = document.getElementById("tSongTitleText6");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle6.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText6.innerHTML;
+    downloadBtn.href = "./audio/trending/Bandana.mp3";
+    downloadAudio.src = "./audio/trending/Bandana.mp3";
+    downloadImg.src = document.getElementById("trendingImg6").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//certified loner
+tdownloadMusic7 = () => {
+
+    let tSongTitle7 = document.getElementById("tSongTitle7");
+    let tSongTitleText7 = document.getElementById("tSongTitleText7");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle7.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText7.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg7").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//Buga
+tdownloadMusic8 = () => {
+
+    let tSongTitle8 = document.getElementById("tSongTitle8");
+    let tSongTitleText8 = document.getElementById("tSongTitleText8");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle8.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText8.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg8").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+
+//
+tdownloadMusic9 = () => {
+
+    let tSongTitle9 = document.getElementById("tSongTitle9");
+    let tSongTitleText9 = document.getElementById("tSongTitleText9");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle9.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText9.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg9").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+tdownloadMusic10 = () => {
+
+    let tSongTitle10 = document.getElementById("tSongTitle10");
+    let tSongTitleText10 = document.getElementById("tSongTitleText10");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle10.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText10.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg10").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+tdownloadMusic11 = () => {
+
+    let tSongTitle11 = document.getElementById("tSongTitle11");
+    let tSongTitleText11 = document.getElementById("tSongTitleText11");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle11.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText11.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg11").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+tdownloadMusic12 = () => {
+
+    let tSongTitle12 = document.getElementById("tSongTitle12");
+    let tSongTitleText12 = document.getElementById("tSongTitleText12");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle12.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText12.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg12").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+tdownloadMusic13 = () => {
+
+    let tSongTitle13 = document.getElementById("tSongTitle13");
+    let tSongTitleText13 = document.getElementById("tSongTitleText13");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle13.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText13.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg13").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+}
+tdownloadMusic14 = () => {
+
+    let tSongTitle14 = document.getElementById("tSongTitle14");
+    let tSongTitleText14 = document.getElementById("tSongTitleText14");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle14.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText14.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg1").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+   }
+
+   tdownloadMusic15 = () => {
+
+    let tSongTitle15 = document.getElementById("tSongTitle15");
+    let tSongTitleText15 = document.getElementById("tSongTitleText15");
+   
+   
+    document.getElementById("downloadH4").innerHTML = tSongTitle15.innerHTML;
+    document.getElementById("downloadP").innerHTML = tSongTitleText15.innerHTML;
+    downloadBtn.href = "./audio/trending/.mp3";
+    downloadAudio.src = "./audio/trending/.mp3";
+    downloadImg.src = document.getElementById("trendingImg15").src;
+   
+   
+    LandingPage.style.display = "none";
+    Recently.style.display = "none";
+    Foreign.style.display = "none";
+    Trending.style.display = "none";
+    navBar.style.display = "none";
+    downloadPage.style.display = "block";
+   }
